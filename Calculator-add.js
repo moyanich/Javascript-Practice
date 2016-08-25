835bd7f00645baf48894d0826c8f0c1a65d68f57
Amoy Nicholson <pixelangel@users.noreply.github.com>
2016-08-25 10:46:50 -0500

    Update

--- Calculator-add.js
+++ Calculator-add.js
@@ -1,1 +1,10 @@
-##Build a function add() which takes two numbers and adds them together.
+/*
+Build a function add() which takes two numbers and adds them together. 
+*/
+
+function add(x, y) {
+
+return x + y;
+
+}
+add(4, 6); //Change values here
